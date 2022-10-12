4b22975949b7804290242d3e49aa56bb80c1ae66
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-10-12 21:42:18 +0200

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,38 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    addPhraseToDisplay() {
+        const div = document.createElement('div');
+        div.id = 'phrase';
+        div.classList.add('section');
+
+        const ul = document.createElement('ul');
+        div.appendChild(ul);
+        
+        this.phrase.forEach( element => {
+            const whiteSpace = /\s/;
+            const li = document.createElement('li');
+            li.classList.add('hide');
+            if (!whiteSpace.test(element)) {
+                li.classList.add('letter');
+            } else {
+                li.classList.add('space');
+            }
+            li.textContent = element;
+            ul.appendChild(li);
+        });
+    }
+}
+
+// Array of phrases
+const phrases = [
+    'test phrase sSSsl',
+];
+turns the phrase into an array of lowercase letters
+const phraseArr = Array.from(phrases[0].toLowerCase())
+
+const newPhrase = new Phrase(phraseArr);
+newPhrase.addPhraseToDisplay();
+console.log(newPhrase);
